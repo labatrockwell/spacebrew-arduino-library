fd5030c3d217df2e7ebd4bf4f9ba12ae9be1e7c2
Quin Kennedy <quinkennedy@LAB-macbookpro-02.rockwellgroup.com>
2012-11-12 20:50:36 -0500

    added comments at the top

--- examples/input_output/input_output.ino
+++ examples/input_output/input_output.ino
@@ -1,4 +1,22 @@
-//#include "Arduino.h"
+/*
+Developed by the LAB at Rockwell Group
+
+There is a .PNG in the examples directory
+that shows how this example is expecting the Arduino to be wired up.
+I have the Ethernet shield sitting on top of the arduino, and then the
+Sparkfun ProtoShield on top of that.
+
+You must remember to include the Ethernet library, SPI library, 
+and the WebSocketClient library 
+(accessible here: https://github.com/labatrockwell/ArduinoWebsocketClient)
+in order to use the Spacebrew library.
+
+visit http://docs.spacebrew.cc/ for more info about Spacebrew!
+
+putting the "toty" back in "Prototyping"
+*/
+
+
 #include <SPI.h>
 #include <Spacebrew.h>
 #include <Ethernet.h>
