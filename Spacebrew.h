603c461f7f84a9ffa114b5ccca02b088c854e079
Quin Kennedy <quinkennedy@LAB-macbookpro-02.rockwellgroup.com>
2012-11-13 11:16:48 -0500

    added example and updated method signatures to be more student-friendly

--- Spacebrew.h
+++ Spacebrew.h
@@ -5,7 +5,7 @@
 #include <WebSocketClient.h>
 #include "Arduino.h"
 
-enum SBType { BOOLEAN, STRING, RANGE };
+enum SBType { SB_BOOLEAN, SB_STRING, SB_RANGE };
 struct PublisherNode {
   char *name;
   char *type;
@@ -23,13 +23,13 @@ class Spacebrew{
   public:
     Spacebrew();
     void monitor();
-    typedef void (*OnBooleanMessage)(char* name, bool value);
-    typedef void (*OnRangeMessage)(char* name, int value);
-    typedef void (*OnStringMessage)(char* name, char* value);
+    typedef void (*OnBooleanMessage)(char name[], bool value);
+    typedef void (*OnRangeMessage)(char name[], int value);
+    typedef void (*OnStringMessage)(char name[], char value[]);
     //typedef void (*OnOtherMessage)(char* name, char* value);//duplicate of OnStringMessage
     typedef void (*OnSBOpen)();
-    typedef void (*OnSBClose)(int code, char* message);
-    typedef void (*OnSBError)(char* message);
+    typedef void (*OnSBClose)(int code, char message[]);
+    typedef void (*OnSBError)(char message[]);
     void onOpen(OnSBOpen function);
     void onClose(OnSBClose function);
     void onRangeMessage(OnRangeMessage function);
@@ -37,50 +37,50 @@ class Spacebrew{
     void onBooleanMessage(OnBooleanMessage function);
     void onOtherMessage(OnStringMessage function);
     void onError(OnSBError function);
-    void addPublish(char* name, char* type, char* defaultValue);
-    void addPublish(char* name, bool defaultValue){
+    void addPublish(char name[], char type[], char defaultValue[]);
+    void addPublish(char name[], bool defaultValue){
       addPublish(name, (char*)"boolean", (char*)(defaultValue ? "true" : "false"));
     }
-    void addPublish(char* name, char* defaultValue){
+    void addPublish(char name[], char defaultValue[]){
       addPublish(name, "string", defaultValue);
     }
-    void addPublish(char* name, int defaultValue);
-    void addPublish(char* name, enum SBType type, char* defaultValue){
+    void addPublish(char name[], int defaultValue);
+    void addPublish(char name[], enum SBType type, char defaultValue[]){
       switch(type){
-        case STRING:
+        case SB_STRING:
           addPublish(name, "string", defaultValue);
           break;
-        case RANGE:
+        case SB_RANGE:
           addPublish(name, "range", defaultValue);
           break;
-        case BOOLEAN:
+        case SB_BOOLEAN:
           addPublish(name, "boolean", defaultValue);
           break;
       }
     }
-    void addPublish(char* name, enum SBType type){
+    void addPublish(char name[], enum SBType type){
       switch(type){
-        case STRING:
+        case SB_STRING:
           addPublish(name, "string", "");
           break;
-        case RANGE:
+        case SB_RANGE:
           addPublish(name, "range", "0");
           break;
-        case BOOLEAN:
+        case SB_BOOLEAN:
           addPublish(name, "boolean", "false");
           break;
       }
     }
-    void addSubscribe(char* name, char* type);
-    void addSubscribe(char* name, enum SBType type){
+    void addSubscribe(char name[], char* type);
+    void addSubscribe(char name[], enum SBType type){
       switch(type){
-        case STRING:
+        case SB_STRING:
           addSubscribe(name, "string");
           break;
-        case RANGE:
+        case SB_RANGE:
           addSubscribe(name, "range");
           break;
-        case BOOLEAN:
+        case SB_BOOLEAN:
           addSubscribe(name, "boolean");
           break;
       }
@@ -91,32 +91,32 @@ class Spacebrew{
     //void addSubscribe(char* name, SBType type, OnBooleanMessage function);
     //void addSubscribe(char* name, SBType type, OnRangeMessage function);
     //void addSubscribe(char* name, SBType type, OnStringMessage function);
-    void connect(char hostname[], char* clientName, char* description, int port = 9000);
+    void connect(char hostname[], char clientName[], char description[], int port = 9000);
     void disconnect();
-    bool send(char* name, char* type, char* value);
-    bool send(char* name, SBType type, char* value){
+    bool send(char name[], char type[], char value[]);
+    bool send(char name[], SBType type, char value[]){
       switch(type){
-        case STRING:
+        case SB_STRING:
           send(name, "string", value);
           break;
-        case RANGE:
+        case SB_RANGE:
           send(name, "range", value);
           break;
-        case BOOLEAN:
+        case SB_BOOLEAN:
           send(name, "boolean", value);
           break;
       }
     }
-    bool send(char* name, char* value){
+    bool send(char name[], char* value){
       send(name, "string", value);
     }
-    bool send(char* name, bool value){
+    bool send(char name[], bool value){
       send(name, (char*)"boolean", (char*)(value ? "true" : "false"));
     }
-    bool send(char* name, boolean value){
+    bool send(char name[], boolean value){
       send(name, (char*)"boolean", (char*)(value ? "true" : "false"));
     }
-    bool send(char* name, int value);
+    bool send(char name[], int value);
     static void onWSError(WebSocketClient client, char* message);//defined in WebSocketClientCallback
     static void onWSOpen(WebSocketClient client);
     static void onWSClose(WebSocketClient client, int code, char* message);
--- examples/button/button.ino
+++ examples/button/button.ino
@@ -24,39 +24,26 @@ putting the "toty" back in "Prototyping"
 
 uint8_t mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 Spacebrew spacebrewConnection;
-int lastAnalog = -1;
 int lastButton = LOW;
 
 const int buttonPin = 7;
-const int digitalLedPin = 8;
-const int analogLedPin = 9;
-const int analogPin = 0;
+const int ledPin = 8;
+const int spacebrew_ledPin = 9;
 
 void setup() {
-  //connect to spacebrew library info
-  spacebrewConnection.onOpen(onOpen);
-  spacebrewConnection.onClose(onClose);
-  spacebrewConnection.onError(onError);
-  
   //connect to message callbacks
   spacebrewConnection.onBooleanMessage(onBooleanMessage);
-  spacebrewConnection.onStringMessage(onStringMessage);
-  spacebrewConnection.onRangeMessage(onRangeMessage);
   
   //register publishers and subscribers
-  spacebrewConnection.addPublish("Analog", SB_RANGE);
   spacebrewConnection.addPublish("Button", SB_BOOLEAN);
-  spacebrewConnection.addPublish("Parrot", SB_STRING);
   spacebrewConnection.addSubscribe("Blink LED", SB_BOOLEAN);
-  spacebrewConnection.addSubscribe("Fade LED", SB_RANGE);
-  spacebrewConnection.addSubscribe("Parrot", SB_STRING);
   
   //connect to the spacebrew server
   Ethernet.begin(mac);
   spacebrewConnection.connect("lab-macbookpro-02.rockwellgroup.com", "Rename_Me", "Arduino Input and Output Test");
   
-  pinMode(digitalLedPin, OUTPUT);
-  pinMode(analogLedPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(spacebrew_ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
 }
 
@@ -65,45 +52,29 @@ void loop() {
   //and re-connect to the server if necessary
   spacebrewConnection.monitor();
   
-  int analogIn = analogRead(analogPin);
-  if (analogIn != lastAnalog){
-    //send a range via spacebrew
-    spacebrewConnection.send("Analog", analogIn);
-    lastAnalog = analogIn;
-  }
-  
   int buttonIn = digitalRead(buttonPin);
   if (buttonIn != lastButton){
+    boolean buttonPressed = (buttonIn == LOW);
+    int ledValue;
+    if (buttonPressed){
+      ledValue = HIGH;
+    } else {
+      ledValue = LOW;
+    }
+    digitalWrite(ledPin, ledValue);
     //send the button state via spacebrew
-    spacebrewConnection.send("Button", buttonIn == LOW);
+    spacebrewConnection.send("Button", buttonPressed);
     lastButton = buttonIn;
   }
 }
 
 void onBooleanMessage(char *name, bool value){
   //turn the 'digital' LED on and off based on the incoming boolean
-  digitalWrite(digitalLedPin, value ? HIGH : LOW);
+  int spacebrew_ledValue;
+  if (value){
+    spacebrew_ledValue = HIGH;
+  } else {
+    spacebrew_ledValue = LOW;
+  }
+  digitalWrite(spacebrew_ledPin, spacebrew_ledValue);
 }
-
-void onStringMessage(char *name, char* message){
-  //repeat back whatever was sent
-  spacebrewConnection.send("Parrot", message);
-}
-
-void onRangeMessage(char *name, int value){
-  //use the range input to control the brightness of the 'analog' LED
-  analogWrite(analogLedPin, map(value, 0, 1024, 0, 255));
-}
-
-void onOpen(){
-  //send a message when we get connected!
-  spacebrewConnection.send("Parrot", "Hello Spacebrew");
-}
-
-void onClose(int code, char* message){
-  //turn everything off if we get disconnected
-  analogWrite(analogLedPin, 0);
-  digitalWrite(digitalLedPin, LOW);
-}
-
-void onError(char* message){}
